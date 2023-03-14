2eca80f99b781f870097d0b9d36545659ce2a175
Felipe Diniz <felipedinizdev@gmail.com>
2023-03-14 02:35:53 -0300

    Utilizando Styled Components

--- src/screens/Meals/index.tsx
+++ src/screens/Meals/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Meals() {
   return (
-    <View style={styles.container}>
-      <Text>Meals</Text>
-    </View>
+    <Container>
+      <Title>Meals</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
