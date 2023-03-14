cbd93dc730e313afd9f774c6edbcaf4cd5548afe
Felipe Diniz <felipedinizdev@gmail.com>
2023-03-14 03:01:32 -0300

    Definindo o tema da aplicação

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/theme';
+
 import { Meals } from '@screens/Meals';
 
 export default function App() {
-  return <Meals />;
+  return;
+  <ThemeProvider theme={theme}>
+    <Meals />;
+  </ThemeProvider>;
 }
